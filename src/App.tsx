9eed3fbfc53760b87cbea89bfdfa4fb56e2cb397
lts-pj <lliamsymonds04@gmail.com>
2025-01-31 17:10:34 +1000

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="flex justify-center items-center  min-h-screen">
+      <p className="text-3xl text-blue-900">App</p>
+    </div>
+  )
+}
+
+export default App
